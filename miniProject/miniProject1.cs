ac38aed137239fdefd0b38b5714d93f63f22a635
sandeep <30605278+Sandeep501@users.noreply.github.com>
2019-07-16 13:57:22 +0530

    Update miniProject1.cs

--- miniProject/miniProject1.cs
+++ miniProject/miniProject1.cs
@@ -5,20 +5,20 @@ public class miniProject1 {
         Console.WriteLine("Enter the no.of test Cases: ");
         int iteration=int.Parse(Console.ReadLine());
         int i=0;
-        while(i<iteration){
-        Console.WriteLine("Enter the String followed by the N and M values:  ");
-        string main=Console.ReadLine();
-        int N=int.Parse(Console.ReadLine());
-        int M=int.Parse(Console.ReadLine());
-        string subStr=main.Substring(N,M);
-        char[] ch=subStr.ToCharArray();
-        Array.Sort(ch);
-        Array.Reverse(ch);
+        while(i<iteration)
+	{
+        	Console.WriteLine("Enter the String followed by the N and M values:  ");
+        	string main=Console.ReadLine();
+     		int N=int.Parse(Console.ReadLine());
+        	int M=int.Parse(Console.ReadLine());
+       		string subStr=main.Substring(N,M);
+        	char[] ch=subStr.ToCharArray();
+        	Array.Sort(ch);
+        	Array.Reverse(ch);
 		string s=string.Join("",ch);
 		string finalRes=main.Replace(subStr,s);
-			
-        Console.WriteLine(finalRes);
-            i++;
+		Console.WriteLine(finalRes);
+       		i++;
         }
         
         
